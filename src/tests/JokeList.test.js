f1a69f2ddb0d70d14db633ef7c53710c83ef0efc
Tor Kingdon <tor@hearkitty.com>
2022-03-01 01:38:42 -0700

    changed tests to work with rewritten snapshots

--- src/tests/JokeList.test.js
+++ src/tests/JokeList.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import JokeList from '../JokeList';
+import { JokeList as StarterJokeList } from '../starter-src/JokeList';
 
 it('should render without crashing', () => {
   render(JokeList);
@@ -8,5 +9,10 @@ it('should render without crashing', () => {
 
 it('should render snapshot', () => {
   const { asFragment } = render();
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it('should match starter code snapshot', () => {
+  const { asFragment } = render(StarterJokeList);
   expect(asFragment()).toMatchSnapshot();
 });
